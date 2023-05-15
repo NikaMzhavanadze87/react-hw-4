b29477143f23ff184a238cfbcfd40fbc235027bb
NikaMzhavanadze87 <n.m.mzhavanadze@gmail.com>
2023-05-16 00:40:28 +0400

    react-hw-4

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+import React, { useState } from "react";
+
+
+function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const getData = async () => {
+    if (inputValue > 3) {
+      try {
+        setLoading(true);
+        setError(null);
+        const response = await axios.get(
+          `https://jsonplaceholder.typicode.com/posts/${inputValue}`
+        );
+        setSearchResults([response.data]);
+      }catch (error) {
+        console.error(error?.message);
+        setError("no such post available.");
+      }finally {
+          setLoading(false);
+        }
+      } else {
+      setSearchResults([]);
+    }
+  };
+  const inputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  return (
+    <div>
+      <input type="text" onChange={inputChange} value={inputValue} />
+      <button onClick={getData}>Get Post</button>
+      <p>{error}</p>
+      
+      <ul>
+        {loading? <p>loading...</p>
+        :
+        searchResults?.map((result) => (
+          <li key={result.id}>{result.body}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
