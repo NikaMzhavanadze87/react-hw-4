08c348bb725dea8394d6ec7ea4d3e754d8adb022
NikaMzhavanadze87 <n.m.mzhavanadze@gmail.com>
2023-05-25 21:56:06 +0400

    react-hw-4

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 
 function App() {
@@ -30,6 +30,9 @@ function App() {
   const inputChange = (e) => {
     setInputValue(e.target.value);
   };
+  useEffect(()=>
+  getData(),[]
+  );
   return (
     <div>
       <input type="text" onChange={inputChange} value={inputValue} />
